eff469adf97ec123978aeaf5795d6f6762379715
Yazitm232 <yazitmbusinessacc12@gmail.com>
2026-03-23 17:49:42 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
+import Admin from './pages/Admin';
 import './index.css';
+
+const isAdmin = window.location.pathname === '/admin';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {isAdmin ? <Admin /> : <App />}
   </StrictMode>
 );
