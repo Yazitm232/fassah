2f0218b9f41e65705ee1ba2b9de96fe7ebb1d9c3
Yazitm232 <yazitmbusinessacc12@gmail.com>
2026-03-23 22:05:31 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App';
 import Admin from './pages/Admin';
 import './index.css';
  
-const isAdmin = window.location.pathname === '/admin';
+const isAdmin = window.location.search.includes('admin=true');
  
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
