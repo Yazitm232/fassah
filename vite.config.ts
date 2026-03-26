2333b9791918db098189f2047551c4efe7d1c0e8
Yazitm232 <yazitmbusinessacc12@gmail.com>
2026-03-26 12:26:30 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
- import { defineConfig } from 'vite'
+
+  import { defineConfig } from 'vite'
   import react from '@vitejs/plugin-react'
+  import { resolve } from 'path'
 
   export default defineConfig({
     plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          app: resolve(__dirname, 'app.html'),
+        }
+      }
+    }
   })
